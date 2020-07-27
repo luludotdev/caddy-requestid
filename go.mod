f9e1c418fb998001e833d59cb250f26f9862f060
Jack Baron <jackmbaron@gmail.com>
2020-07-27 20:53:40 +0100

    write the module

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/lolPants/caddy-reqid
+
+go 1.14
+
+require (
+	github.com/caddyserver/caddy/v2 v2.1.1
+	github.com/google/uuid v1.1.1
+)
---
+++ main.go
@@ -0,0 +1,96 @@
+package reqid
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/google/uuid"
+)
+
+func init() {
+	caddy.RegisterModule(Middleware{})
+	httpcaddyfile.RegisterHandlerDirective("request_id", parseCaddyfile)
+}
+
+// Middleware implements an HTTP handler that writes a
+// unique request ID to response headers.
+type Middleware struct {
+	// The name of the header to write to.
+	Header string `json:"header,omitempty"`
+
+	h string
+}
+
+// CaddyModule returns the Caddy module information.
+func (Middleware) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.request_id",
+		New: func() caddy.Module { return new(Middleware) },
+	}
+}
+
+// Provision implements caddy.Provisioner.
+func (m *Middleware) Provision(ctx caddy.Context) error {
+	m.h = m.Header
+
+	if m.h == "" {
+		m.h = "x-request-id"
+	}
+
+	return nil
+}
+
+// Validate implements caddy.Validator.
+func (m *Middleware) Validate() error {
+	if m.h == "" {
+		return fmt.Errorf("no header")
+	}
+
+	return nil
+}
+
+// ServeHTTP implements caddyhttp.MiddlewareHandler.
+func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	uid := uuid.New().String()
+	w.Header().Set(m.h, strings.ReplaceAll(uid, "-", ""))
+
+	return next.ServeHTTP(w, r)
+}
+
+// UnmarshalCaddyfile implements caddyfile.Unmarshaler.
+func (m *Middleware) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+		if d.NextArg() {
+			// optional arg
+			m.Header = d.Val()
+		}
+
+		if d.NextArg() {
+			// too many args
+			return d.ArgErr()
+		}
+	}
+
+	return nil
+}
+
+// parseCaddyfile unmarshals tokens from h into a new Middleware.
+func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
+	var m Middleware
+	err := m.UnmarshalCaddyfile(h.Dispenser)
+
+	return m, err
+}
+
+// Interface guards
+var (
+	_ caddy.Provisioner           = (*Middleware)(nil)
+	_ caddy.Validator             = (*Middleware)(nil)
+	_ caddyhttp.MiddlewareHandler = (*Middleware)(nil)
+	_ caddyfile.Unmarshaler       = (*Middleware)(nil)
+)
